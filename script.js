0b657db0e461db92fa509cc2b803787a7756b52c
Ankit Prajapati <ankitprajapati.dev29@gmail.com>
2026-09-03 01:42:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", () => {
        CONFIG
     ====================================================== */
 
-    const API_BASE = const API_BASE = "https://securedoc-ai-t5m0.onrender.com";
+    const API_BASE = "https://securedoc-ai-t5m0.onrender.com";
     const UPLOAD_ENDPOINT =
         `${API_BASE}/upload`;
 
